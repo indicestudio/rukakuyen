57adaa48f0eb0bc6b1ce1316d43140f87b17fc3b
indicestudio <indicestudio@gmail.com>
2018-01-13 00:10:22 -0300

    initial commit

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+    /* GSAP Animations */
+    /* Loading Animation */
+    
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
